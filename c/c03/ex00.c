b40429ef7d12ceabf28897406a9450b071d30c58
Yasmine Kara <yasmine92007@gmail.com>
2026-07-03 12:22:42 +0200

    add c03

---
+++ c/c03/ex00.c
@@ -0,0 +1,10 @@
+//ft_strcmp
+int     ft_strcmp(char *s1, char *s2)
+{
+    while (*s1 == *s2 && *s1 != '\0')
+    {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
+}
---
+++ c/c03/ex02.c
@@ -0,0 +1,19 @@
+//ft_strcat
+char	*ft_strcat(char *dest, char *src)
+{
+	int		c;
+	int		d;
+
+	c = 0;
+	d = 0;
+	while (dest[c] != '\0')
+		c++;
+	while (src[d] != '\0')
+	{
+		dest[c] = src[d];
+		c++;
+		d++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
