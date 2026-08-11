e0416acbf3175ec8b2259060f092ea53ef1b99c0
Yasmine Kara <yasmine92007@gmail.com>
2026-08-11 16:48:22 +0200

    add c06

---
+++ c/c06/ex00.c
@@ -0,0 +1,21 @@
+//ft_print_program_name
+#include <unistd.h>
+
+int     main(int argc, char **argv)
+{
+	int	i;
+    i = 0;
+
+	(void)argc;
+
+	while (argv[0][i])
+    {
+		i++;
+    }
+
+	write(1, argv[0], i);
+
+	write(1, "\n", 1);
+    
+	return (0);
+}
---
+++ c/c06/ex02.c
@@ -0,0 +1,26 @@
+//ft_sort_params
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i--;
+	}
+	return (0);
+}
